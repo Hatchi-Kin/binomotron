ba935bb7bdc0dfbe419df0335b9a8e15795b4b35
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-05-15 19:25:36 +0200

    Update binomo_v2.py

--- binomo_v2.py
+++ binomo_v2.py
@@ -12,13 +12,14 @@ bdd = msqlpy.connect(user=user, password=password, host=host, port=port, databas
 cursor = bdd.cursor()
 
 #################################
-
-liste_noms = creer_liste_noms()    # fonction perso pour récuperer la colomne prénom dans la table apprenants de la BDD et en faire une liste
-liste_noms_deux = creer_liste_noms() # au cas ou j'utilise la fonction shuffle qui modifierai ma liste_noms, cette nouvelle variable restera dans l'ordre
+# fonction perso pour récuperer la colonne prénom dans la table apprenants de la BDD et en faire une liste
+liste_noms = creer_liste_noms()
+# au cas ou j'utilise la fonction shuffle qui modifierai ma liste_noms, cette nouvelle variable restera dans l'ordre
+liste_noms_deux = creer_liste_noms()
 on = True
 
-
-while on:                # tant que la variable on est True, on reste dans la boucle (on devient faux si user entre "x" dans le menu)
+# tant que la variable on est True, on reste dans la boucle (on devient faux si user entre "x" dans le menu)
+while on:
     binomes = creer_binomes(liste_noms)
     action = input("""
 
@@ -31,8 +32,8 @@ while on:                # tant que la variable on est True, on reste dans la bo
     **********************************************************************************************
     """)
 
-    
-    if action == "a":                         # condition pour afficher une nouvelle liste aléatoire de binomes
+    # condition pour afficher une nouvelle liste aléatoire de binomes
+    if action == "a":
         liste_noms = creer_liste_noms()
         binomes = creer_binomes(liste_noms)
         print(binomes)
@@ -43,9 +44,11 @@ while on:                # tant que la variable on est True, on reste dans la bo
     **********************************************************************************************
     
         # """)
-        if in_action == "n":                   # dans la condition "a", condition pour ne pas sauvegarder la liste dans la BDD et revenir au menu
+        # dans la condition "a", condition pour ne pas sauvegarder la liste dans la BDD et revenir au menu
+        if in_action == "n":
             continue
-        elif in_action == "y":                 # dans "a", condition pour sauvegarder (après confirmlation) la liste dans la BDD
+        # dans "a", condition pour sauvegarder (après confirmlation) la liste dans la BDD
+        elif in_action == "y":
             new_input = input("""
     **********************************************************************************************
     Attention, ceci effacera tous les binomes déjà enregistrés s'ils existent ! confirmer? (y/n): 
@@ -54,40 +57,45 @@ while on:                # tant que la variable on est True, on reste dans la bo
             if new_input == "n":
                 continue
             elif new_input == "y":
-                commit_binomes_to_bdd(binomes)   # fonction perso pour effacer le contenu de la table binomes puis enregistrer la liste précedement créée
+                # fonction perso pour effacer le contenu de la table binomes puis enregistrer la liste précedement créée
+                commit_binomes_to_bdd(binomes) 
             else:
                 print("Veuillez entrez une commande valide. ")
                 continue
 
 
     elif action == "b":
-            print(show_duos_from_bdd())        # fonction perso créer une liste avec le contenu de la colonne libelle_buo de la table duos de la BDD
+            # fonction perso créer une liste avec le contenu de la colonne libelle_buo de la table duos de la BDD
+            print(show_duos_from_bdd())
 
-
-    elif action.isdigit() and int(action) >= 1 and int(action) <= 8:     # condition pour afficher un binome spécifique (input = indice de la liste binomes[])
+    # condition pour afficher un binome spécifique (input = indice de la liste binomes[])
+    elif action.isdigit() and int(action) >= 1 and int(action) <= 8: 
             binomes = show_duos_from_bdd()
             hot_bin = binomes[(int(action)-1)]
             print(f"binome_{int(action)}: {hot_bin}")
 
-
-    elif action in liste_noms_deux:            # condition pour afficher binome + mails
+    # condition pour afficher binome + mails
+    elif action in liste_noms_deux:          
         liste_mails = creer_liste_mail()
         duos_from_bdd = (show_duos_from_bdd())
         for i in duos_from_bdd:
-            for i in duos_from_bdd:           # boucle pour trouver le nom demandé dans liste  binomes[] qui contient les binomes sous forme de string
-                if i.find(action) == 0:       # si le prénom est trouvé et se trouve au début de la string
+            # boucle pour trouver le nom demandé dans liste  binomes[] qui contient les binomes sous forme de string
+            for i in duos_from_bdd: 
+                # si le prénom est trouvé et se trouve au début de la string
+                if i.find(action) == 0: 
                     action_binome = i.split(" ")[-1]
-                elif i.find(action) > 0:      # si le prénom est trouvé et se trouve à la fin de la string
+                # si le prénom est trouvé et se trouve à la fin de la string
+                elif i.find(action) > 0: 
                     action_binome = i.split(" ")[0]
         print(f"l'adresse mail de {action} est:  {liste_mails[action]}")
         print(f"Son binome est: {action_binome} et son mail est:  {liste_mails[action_binome]}")
             
-
-    elif action == "x":                           # condition pour QUITTER le programme
+    # condition pour QUITTER le programme
+    elif action == "x":
         on = False
     
-    
-    else:                         # si la commande entrée ne correspond à aucune conditions gerées par notre script
+    # si la commande entrée ne correspond à aucune conditions gerées par notre script
+    else:  
         print("Veuillez entrez une commande valide. ")
         continue
 
