b691eae117ab183d14ab5c1029e276eb303746db
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-05-17 11:58:25 +0200

    Update binomo_v2_fonctions.py

--- binomo_v2_fonctions.py
+++ binomo_v2_fonctions.py
@@ -15,6 +15,7 @@ def creer_liste_noms():
     cursor.execute(query)
 
     for eleve in cursor:
+        # Dans cursor, la colonne avec les prénoms est la deuxième colonne, donc on récupère eleve[1]
         liste_noms.append(eleve[1])
     return liste_noms
 ################################################
@@ -29,6 +30,21 @@ def creer_binomes(liste_noms):
         liste_noms.remove(nom2)
         binomes.append(f"{nom1} et {nom2}")
     return binomes
+################################################
+
+################################################
+def creer_groupe_de_n(choix_de_n):
+
+    liste_prenoms = creer_liste_noms()
+    reste = len(liste_prenoms) % int(choix_de_n)
+    groupe_de_n = []
+    random.shuffle(liste_prenoms)
+
+    for i in range(0, len(liste_prenoms) - reste, choix_de_n):
+        groupe_de_n.append(liste_prenoms[i:i+choix_de_n])
+    if reste > 0:
+        groupe_de_n.append(liste_prenoms[-reste:])
+    return groupe_de_n
 ################################################
 
 ################################################
@@ -84,4 +100,26 @@ def creer_liste_mail():
         liste_noms.append(eleve[0])
         liste_mails[eleve[0]] = eleve[1]
     return liste_mails
+################################################
+
+################################################
+def commit_groupe_de_n_to_bdd(groupe_de_n):
+    user = "root"
+    password = "example"
+    host = "localhost"
+    port = "3307"
+    database = "binomotron_t2_bdd"
+    bdd = msqlpy.connect(user=user, password=password, host=host, port=port, database=database)
+    cursor = bdd.cursor()
+    query = "DELETE FROM groupes"
+    cursor.execute(query)
+    bdd.commit()
+
+    groupe_a_vider = groupe_de_n
+    
+    for i in groupe_a_vider:
+        j = ', '.join(i)
+        query = f"INSERT INTO groupes (libelle_groupe) VALUES ('{j}')"
+        cursor.execute(query)
+        bdd.commit()
 ################################################
